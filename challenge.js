44af273c19bab301e922d3c04e7862b4604f3720
Juan Diego Guerra <jdxelric@gmail.com>
2023-05-06 01:57:37 -0500

    Update challenge.js
    Solution of coding challenge

--- challenge.js
+++ challenge.js
@@ -1,6 +1,44 @@
 function balancedBrackets(expr) {
   //your code here
+  let stack = [];
 
+  for(let i = 0; i < expr.length; i++)
+  {
+      let x = expr[i];
+
+      if (x == '(' || x == '[' || x == '{')
+      {
+          stack.push(x);
+          continue;
+      }
+
+      if (stack.length == 0)
+          return false;
+            
+      let check;
+      switch (x){
+      case ')':
+          check = stack.pop();
+          if (check == '{' || check == '[')
+              return false;
+          break;
+
+      case '}':
+          check = stack.pop();
+          if (check == '(' || check == '[')
+              return false;
+          break;
+
+      case ']':
+          check = stack.pop();
+          if (check == '(' || check == '{')
+              return false;
+          break;
+      }
+  }
+
+  // Check Empty Stack
+  return (stack.length == 0);
 }
 
 console.log(balancedBrackets('(hello)[world]'))
